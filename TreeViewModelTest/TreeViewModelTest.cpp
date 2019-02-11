9b9c0f8d08b4ed21eaa26f8be7de935797ce9f7d
Sandeep Kinra <Sandeep.Kinra@doculynx.com>
2019-02-10 18:15:57 -0700

    Added another googleTest case.

--- TreeViewModelTest/TreeViewModelTest.cpp
+++ TreeViewModelTest/TreeViewModelTest.cpp
@@ -42,18 +42,29 @@ public:
 
 	ItemTreeModel* _model = nullptr;
 	bool _status = false;
+	bool _skiptest = false;
 };
 
 
 // test if json data is loaded ok.
 TEST_F(TreeViewModelTest, loadJson)
 {
+	if (_skiptest)
+		return;
+
+	qDebug() << "Testing loadJson " << "\n";
+
 	ASSERT_TRUE(_status);
 }
 
 // verify the Node A is parent
 TEST_F(TreeViewModelTest, treeViewModel_parent)
 {
+	if (_skiptest)
+		return;
+
+	qDebug() << "Testing treeViewModel_parent " << "\n";
+
 	TreeItem* parent = _model->nodeForIndex(QModelIndex());
 	int childCount = parent->childCount();
 
@@ -66,6 +77,11 @@ TEST_F(TreeViewModelTest, treeViewModel_parent)
 // Verify that A nodes has 2 children
 TEST_F(TreeViewModelTest, treeViewModel_childCount)
 {
+	if (_skiptest)
+		return;
+
+	qDebug() << "Testing treeViewModel_childCount " << "\n";
+
 	QModelIndex parent = _model->index(0, 0, QModelIndex());
 	int childCount = _model->rowCount(parent);
 
@@ -75,6 +91,11 @@ TEST_F(TreeViewModelTest, treeViewModel_childCount)
 // Remove Node C
 TEST_F(TreeViewModelTest, removeNode)
 {
+	if (_skiptest)
+		return;
+
+	qDebug() << "Testing removeNode " << "\n";
+
 	QModelIndex parent = _model->index(0, 0, QModelIndex());
 	QVariant data_b = _model->index(0, 0, parent).data();
 	QVariant data_c = _model->index(1, 0, parent).data();
@@ -87,6 +108,50 @@ TEST_F(TreeViewModelTest, removeNode)
 	int childCount = _model->rowCount(parent);
 
 	ASSERT_EQ(childCount, 1);
+}
+
+// This make C as child of B.
+TEST_F(TreeViewModelTest, MoveNode)
+{
+	if (_skiptest)
+		return;
+
+	qDebug() << "Testing MoveNode " << "\n";
+
+	QModelIndex parent = _model->index(0, 0, QModelIndex());
+	QModelIndex idx_b = _model->index(0, 0, parent);
+	QVariant data_b = idx_b.data();
+
+	// let move C
+	QModelIndex idx_c = _model->index(1, 0, parent);
+	QVariant data_c = idx_c.data();
+
+	//QList<QModelIndex> _list
+	QModelIndexList _list;
+	_list.append(idx_c);
+	QMimeData* mimeData = _model->mimeData(_list);
+	_model->dropMimeData(mimeData, Qt::DropAction::MoveAction, -1, -1, idx_b);
+
+	// Now verify if C is child of B.
+	QModelIndex idx_ua = _model->index(0, 0, QModelIndex());
+	QVariant data_ua = idx_ua.data();
+	QModelIndex idx_ub = _model->index(0, 0, idx_ua);
+	QVariant data_ub = idx_ub.data();
+
+	// check childcount from B.
+	TreeItem* bnode = _model->nodeForIndex(idx_ub);
+	ASSERT_EQ(bnode->childCount(), 1);
+
+	QModelIndex idx_uc = _model->index(0, 0, idx_ub);
+	QVariant data_uc = idx_uc.data();
+
+	// Check if child is actually 'C' node
+	TreeItem* cnode = _model->nodeForIndex(idx_uc);
+
+	int cmpStatus = QString::compare(cnode->getKey(), QString("C"));
+
+	ASSERT_EQ(cmpStatus, 0);
+
 }
 
 int main(int argc, char **argv) 
